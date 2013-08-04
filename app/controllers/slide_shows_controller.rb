bca5741c3b0cb58fb9b145f99a24df0c67f3aa0e
Feynman Liang <feynman.liang@gmail.com>
2013-08-03 17:40:04 -0700

    Nested slides under slideShows

--- app/controllers/slide_shows_controller.rb
+++ app/controllers/slide_shows_controller.rb
@@ -14,6 +14,7 @@ class SlideShowsController < ApplicationController
   # GET /slide_shows/1.json
   def show
     @slide_show = SlideShow.find(params[:id])
+    @slides = @slide_show.slides
 
     respond_to do |format|
       format.html # show.html.erb
