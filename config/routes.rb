67139d72147acaad905d8de5bc307cc4845482a9
Feynman Liang <feynman.liang@gmail.com>
2013-08-03 18:42:50 -0700

    Added basic session handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Memoir::Application.routes.draw do
+  root :to => "home#index"
   resources :slide_shows, only: [:show, :new, :create]
+  match '/auth/:provider/callback' => 'sessions#create'
+  match '/signin' => 'sessions#new', :as => :signin
+  match '/signout' => 'sessions#destroy', :as => :signout
+  match '/auth/failure' => 'sessions#failure'
 end
