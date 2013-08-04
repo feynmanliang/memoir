1d473e14b65bd7c4e526ce0014f6db4d25d2cec3
Feynman Liang <feynman.liang@gmail.com>
2013-08-03 18:11:08 -0700

    index on master: e04dbf3 Remove Slide resource route

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130803235944) do
+ActiveRecord::Schema.define(:version => 20130804004351) do
 
   create_table "slide_shows", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.string   "color1"
+    t.string   "color2"
+    t.string   "description"
   end
 
   create_table "slides", :force => true do |t|
